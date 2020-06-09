82b075a3ec6a53ef7fa672f1a58e880ffc1c8215
root <ayonalmamun@gmail.com>
2020-06-09 22:47:54 +0000

    ui bs4 upgrade

--- app.js
+++ app.js
@@ -56,10 +56,7 @@ app.get("/", function(req, res){
 	res.render("home");
 });
 
-app.get("/about", function(req, res){
-		
-	res.render("about");
-});
+
 
 //INDEX
 app.get("/flats", function(req, res){
